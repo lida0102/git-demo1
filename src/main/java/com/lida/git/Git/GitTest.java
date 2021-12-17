d00e8e87e23f01f00bbc31047959c6af51ec0715
lida <lida@qq.com>
2021-12-17 14:12:35 +0800

    version2

--- src/main/java/com/lida/git/Git/GitTest.java
+++ src/main/java/com/lida/git/Git/GitTest.java
@@ -3,5 +3,7 @@ package com.lida.git.Git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello world");
+        System.out.println("hello lida");
     }
 }
